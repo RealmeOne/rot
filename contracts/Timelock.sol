6011c6449f3fd3a95fcfc60ed055e700fed0da2a
ashimida6 <ashimida6@github.com>
2018-09-03 16:36:48 +0800

    timelock test with remix, already worked

--- contracts/Timelock.sol
+++ contracts/Timelock.sol
@@ -1,35 +1,41 @@
 pragma solidity 0.4.24;
 
-interface ERC20Token {
+interface IERC20 {
     function transfer(address _to, uint256 _value) external returns (bool success);
     function balanceOf(address _owner) external view returns (uint256 balance);
 }
 
+library SafeERC20 {
+  function safeTransfer(IERC20 _token, address _to, uint256 _value) internal {
+    require(_token.transfer(_to, _value));
+  }
+}
+
 contract Timelock {
-    ERC20Token public token;
+    using SafeERC20 for IERC20;
+    IERC20 public token;
     address public beneficiary;
     uint256 public releaseTime;
 
     event TokenReleased(address beneficiary, uint256 amount);
 
     constructor(
-        address _token,
-        address _beneficiary
-        // ,
-        // uint256 _releaseTime
+        IERC20 _token,
+        address _beneficiary,
+        uint256 _releaseTime
     ) public {
-        // require(_releaseTime > now);
+        require(_releaseTime > now);
         require(_beneficiary != 0x0);
-        token = ERC20Token(_token);
+        token = _token;
         beneficiary = _beneficiary;
-        // releaseTime = _releaseTime;
+        releaseTime = _releaseTime;
     }
 
     function release() public returns(bool success) {
-        // require(now >= releaseTime);
-        uint256 amount = token.balanceOf(this);
+        require(now >= releaseTime);
+        uint256 amount = token.balanceOf(address(this));
         require(amount > 0);
-        token.transfer(beneficiary, amount);
+        token.safeTransfer(beneficiary, amount);
         emit TokenReleased(beneficiary, amount);
         return true;
     }
